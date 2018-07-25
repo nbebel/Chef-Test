6cf2fe035e3a98e7b476bb4d806692e635658676
nbebel <nbebel@nasuni.com>
2018-07-25 15:42:08 -0400

    update

--- mongo/metadata.rb
+++ mongo/metadata.rb
@@ -1,8 +1,13 @@
 name              'mongodb'
 maintainer        'edelight GmbH'
 maintainer_email  'markus.korn@edelight.de'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs and configures mongodb'
 version           '0.16.3'
 
 depends 'yum', '>= 3.0'
+depends 'mongo'
+
+redhat.each do |os|
+	supports os
+end
