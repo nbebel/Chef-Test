c32ad8b8d805e1540d90c9e21f5fd3a627323e21
nbebel <nbebel@nasuni.com>
2018-07-25 08:48:15 -0400

    adding cookbooks

---
+++ mongo/metadata.rb
@@ -0,0 +1,8 @@
+name              'mongodb'
+maintainer        'edelight GmbH'
+maintainer_email  'markus.korn@edelight.de'
+license           'Apache 2.0'
+description       'Installs and configures mongodb'
+version           '0.16.3'
+
+depends 'yum', '>= 3.0'
